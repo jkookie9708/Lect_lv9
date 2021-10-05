dd885794d2d5df191ce8b94d63396506dda0d60c
jkookie9708 <86417148+jkookie9708@users.noreply.github.com>
2021-10-05 14:54:42 +0900

    Start RPG Project

---
+++ RPG/src/controller/Main.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class Main {
+	
+	public void MainGame() {
+		boolean isRun = true;
+		while(isRun) {
+			GameTitle();
+			
+		}
+	}
+	
+	private void GameTitle() {
+		System.out.println("╊─━──━━───━──━━──╉");
+		System.out.println("│ asdfasdfasdfas │");
+		System.out.println("╉━──━━───━───━──━╉");
+	}
+	
+	private void GameStartMenu() {
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
