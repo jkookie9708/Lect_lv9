026060263fb18348992e2e465f84278c5b672648
jkookie9708 <86417148+jkookie9708@users.noreply.github.com>
2021-10-06 09:51:06 +0900

    1

--- RPG/src/controller/Main.java
+++ RPG/src/controller/Main.java
@@ -1,11 +1,17 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	Scanner sc = new Scanner(System.in);
 	
 	public void MainGame() {
 		boolean isRun = true;
 		while(isRun) {
 			GameTitle();
+			GameStartMenu();
+			GameStartSelec();
 			
 		}
 	}
@@ -17,12 +23,24 @@ public class Main {
 	}
 	
 	private void GameStartMenu() {
-		System.out.println();
+		System.out.println("1] 새로운모험 시작");
+		System.out.println("2] 불러오기");
+		System.out.println("3] 종   료");
 	}
 
+	private void GameStartSelec() {
+		boolean isRun = true;
+		
+			System.out.print("->");
+			System.out.print("  ");
+		
+		String start = sc.next();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Main game = new Main();
+		game.MainGame();
 	}
 
 }
