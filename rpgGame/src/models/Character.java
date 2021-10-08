34bada81b8754bc336f06a8e8257c0658761c7ff
jkookie9708 <86417148+jkookie9708@users.noreply.github.com>
2021-10-08 13:27:50 +0900

    ㅅㅅ

---
+++ rpgGame/src/models/Character.java
@@ -0,0 +1,49 @@
+package models;
+
+public class Character {
+	
+	private int level;
+	private String gender; 
+	private String name;
+	private int age;
+	private int gold;
+	
+	// ability
+	private int STR;
+	private int AGI;
+	private int INT;
+	
+	
+	public Character(String gender, String name, int age, int STR, int AGI, int INT) {
+		this.level = 1;
+		this.gender = gender;
+		this.name = name;
+		this.age = age;
+		this.STR = STR;
+		this.AGI = AGI;
+		this.INT = INT;
+		this.gold = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
