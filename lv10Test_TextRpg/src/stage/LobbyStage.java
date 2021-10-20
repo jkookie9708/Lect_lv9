86ae683091e764029a3963f8ffb6858d1706ae39
jkookie9708 <86417148+jkookie9708@users.noreply.github.com>
2021-10-20 23:37:24 +0900

    re

---
+++ lv10Test_TextRpg/src/stage/LobbyStage.java
@@ -0,0 +1,18 @@
+package stage;
+
+public class LobbyStage extends BasicsStage{
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
