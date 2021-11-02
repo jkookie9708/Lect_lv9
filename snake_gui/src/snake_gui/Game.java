e68178255608a70ef241ee5e61473d772ca5ef43
baekjongguk <baekjongguk@localhost>
2021-11-02 13:15:00 +0900

    re

---
+++ snake_gui/src/snake_gui/Game.java
@@ -0,0 +1,29 @@
+package snake_gui;
+
+import javax.swing.JFrame;
+
+class SnakePanel extends MyUtil {
+	
+	private Rect[][] map;
+	
+	private int size;
+	private Rect[] snake;
+	
+	public SnakePanel() {
+		
+	}
+}
+
+public class Game extends JFrame {
+
+	private SnakePanel panel = new SnakePanel();
+	
+	public Game() {
+		super("Snake Game");
+		setLayout(null);
+		setBounds(50, 50, 700, 700);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+}
